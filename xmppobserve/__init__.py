5ff86682e86ee4dccd1eb721f7bf5135dcc1c1c3
Jonas Schäfer <j.wielicki@sotecware.net>
2020-04-25 13:44:55 +0200

    Add license note

--- xmppobserve/__init__.py
+++ xmppobserve/__init__.py
@@ -6,6 +6,12 @@ from quart import (
 
 app = Quart(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 async def index():
     return await render_template("index.html")
+
+
+@app.route("/LICENSE", methods=["GET"])
+async def license():
+    return await render_template("license.html")
